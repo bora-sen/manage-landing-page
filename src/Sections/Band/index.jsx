3b6f3b207e33ae371831889c5ba80b7a14bd9ef1
bora-sen <bahattinborasen@gmail.com>
2023-02-27 16:47:27 +0300

    +added section -> Band

---
+++ src/Sections/Band/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Band() {
+  return (
+    <section className="bg-primary-red text-white h-auto flex flex-col items-center gap-8 py-12 md:py-12 ">
+      <h6 className='font-bold text-7xl text-center leading-[4.3rem]'>Simplify how your team works today.</h6>
+      <button className='text-primary-red bg-white px-8 py-4 text-2xl font-bold rounded-full'>Get Started</button>
+    </section>
+  )
+}
+
+export default Band
